624b2004e81ffccc792a329404226c1e07fc477e
Cristian García <cgardev@gmail.com>
2017-07-21 20:03:27 +0200

    Renamed folders

--- code/config/settings.py
+++ code/config/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'website.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -78,7 +78,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'website.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
